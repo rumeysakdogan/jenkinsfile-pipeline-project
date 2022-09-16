61230dc19dfa833727a9b64efd639464d495cbc2
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-09-15 22:03:48 -0400

    updated jenkinsfile and added Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Java,configuring 
 jenkins with java and maven");
+	System.out.println("\(-_-)/");
     }
 }
