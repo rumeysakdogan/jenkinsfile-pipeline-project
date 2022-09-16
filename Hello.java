860755808180d261b57e9617e4b62284fed301fd
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-09-15 22:01:45 -0400

    updated jenkinsfile and added Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java");
+        System.out.println("Hello from Java,configuring 
+jenkins with java and maven");
     }
 }
