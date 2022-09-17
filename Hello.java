935318a9590cada11627135d4b099e464928d323
EC2 Default User <ec2-user@ip-172-31-82-180.ec2.internal>
2022-09-17 15:35:31 +0000

    updated Jenkinsfile

--- Hello.java
+++ Hello.java
@@ -2,6 +2,5 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello from Java,configuring jenkins with java and maven");
-	System.out.println("another line added");
     }
 }
